f30da04931dee5aa43c4d3edf03a6e6c16324587
Teddy Milon <milonte@gmail.com>
2018-09-25 10:45:23 +0200

    correction

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
 
-require __DIR__ . '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use HelloWorld\SayHello;
+
+$hello = new HelloWorld\SayHello();
+echo $hello->world();
 
 echo SayHello::world();
